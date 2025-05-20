be89005d8741405f4fd9686e37461887716ce794
glavadoj01 <gonzalolj@hotmail.es>
2025-05-20 10:17:54 +0200

    dia 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', // localhost:4200
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)        
+    }
+]
